ae70218869fc8075f1ae5e0793adc945522c6a9f
calixto <calixxtorafaell@gmail.com>
2023-08-29 16:26:24 -0300

    add pathVariable

--- demo/src/main/java/com/dsenvolvendosistemas/demo/rest/StudentRestController.java
+++ demo/src/main/java/com/dsenvolvendosistemas/demo/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.dsenvolvendosistemas.demo.rest;
 
 import com.dsenvolvendosistemas.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,15 +13,22 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    @GetMapping("/students")
-    public List<Student> getStudents() {
+    private List<Student> theStudents;
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
 
-        List<Student> theStudents = new ArrayList<>();
-        
         theStudents.add(new Student("Rafael", "Calixto"));
         theStudents.add(new Student("Ronoroa", "Zoro"));
         theStudents.add(new Student("Ronaldinho", "Gaucho"));
-
+    }
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return theStudents;
+    }
+    @GetMapping("/student/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+
+        return theStudents.get(studentId);
     }
 }
