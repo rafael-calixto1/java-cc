11739054ea1f1f19d4908c65c6ddb81a86f9a5b7
calixto <calixxtorafaell@gmail.com>
2023-09-01 16:46:32 -0300

    add update, delete and save methods to DAO

--- cruddemo/cruddemo/src/main/java/com/dsenvolvendosistemas/cruddemo/dao/EmployeeDAOJpaImpl.java
+++ cruddemo/cruddemo/src/main/java/com/dsenvolvendosistemas/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -27,5 +27,30 @@ public class EmployeeDAOJpaImpl implements EmployeeDAO{
         return employees;
     }
 
+    @Override
+    public Employee findById(int theId) {
+        //get employee
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+        //return employee
+        return theEmployee;
+    }
+
+    @Override
+    public Employee save(Employee theEmployee) {
+        //save employee
+        Employee dbEmployee = entityManager.merge(theEmployee); //if id ==o then insert/save else update
+
+        //return the dbEmployee
+        return dbEmployee;
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        //find employee
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+        //remove employee
+        entityManager.remove(theEmployee)
+    }
+
 
 }
