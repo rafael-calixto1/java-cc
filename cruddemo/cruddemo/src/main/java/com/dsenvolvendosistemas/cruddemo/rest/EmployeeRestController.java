ae93cae68ef87e218fa9a6e56533bc926743aeee
calixto <calixxtorafaell@gmail.com>
2023-09-01 16:19:45 -0300

    refacored the code and add a service layer

--- cruddemo/cruddemo/src/main/java/com/dsenvolvendosistemas/cruddemo/rest/EmployeeRestController.java
+++ cruddemo/cruddemo/src/main/java/com/dsenvolvendosistemas/cruddemo/rest/EmployeeRestController.java
@@ -2,6 +2,7 @@ package com.dsenvolvendosistemas.cruddemo.rest;
 
 import com.dsenvolvendosistemas.cruddemo.dao.EmployeeDAO;
 import com.dsenvolvendosistemas.cruddemo.entity.Employee;
+import com.dsenvolvendosistemas.cruddemo.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +12,15 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
-    private EmployeeDAO employeeDAO;
+    private EmployeeService employeeService;
 
     //quick and dirty: inject employee dao using constructor injection
-    public EmployeeRestController(EmployeeDAO theEmployeeDAO){
-        employeeDAO = theEmployeeDAO;
+    public EmployeeRestController(EmployeeService theEmployeeService){
+
+        employeeService = theEmployeeService;
     }
     @GetMapping("/employees")
     public List<Employee> findAll(){
-        return employeeDAO.findAll();
+        return employeeService.findAll();
     }
 }
--- cruddemo/cruddemo/src/main/java/com/dsenvolvendosistemas/cruddemo/service/EmployeeServiceImpl.java
+++ cruddemo/cruddemo/src/main/java/com/dsenvolvendosistemas/cruddemo/service/EmployeeServiceImpl.java
@@ -1,2 +1,22 @@
-package com.dsenvolvendosistemas.cruddemo.service;public class EmployeeServiceImpl {
+package com.dsenvolvendosistemas.cruddemo.service;
+
+import com.dsenvolvendosistemas.cruddemo.dao.EmployeeDAO;
+import com.dsenvolvendosistemas.cruddemo.dao.EmployeeDAOJpaImpl;
+import com.dsenvolvendosistemas.cruddemo.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+    private EmployeeDAO employeeDAO;
+    @Autowired
+    public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO){
+        employeeDAO = theEmployeeDAO;
+    }
+    @Override
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
 }
