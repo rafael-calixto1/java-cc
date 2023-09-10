70c40938ecbdb839b568037f2c21f401828a2d3e
rafael-calixto <calixxtorafaell@gmail.com>
2023-09-10 18:06:00 -0300

    add CRUD

--- employeemanagement/src/main/java/com/dsenvolvendosistemas/employeemanagement/dao/EmployeeDAO.java
+++ employeemanagement/src/main/java/com/dsenvolvendosistemas/employeemanagement/dao/EmployeeDAO.java
@@ -1,9 +1,18 @@
 package com.dsenvolvendosistemas.employeemanagement.dao;
 
 import com.dsenvolvendosistemas.employeemanagement.entity.Employee;
+import java.util.List;
 
 import java.util.List;
 
+
 public interface EmployeeDAO {
+
     List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
 }
--- employeemanagement/src/main/java/com/dsenvolvendosistemas/employeemanagement/service/EmployeeService.java
+++ employeemanagement/src/main/java/com/dsenvolvendosistemas/employeemanagement/service/EmployeeService.java
@@ -5,5 +5,13 @@ import com.dsenvolvendosistemas.employeemanagement.entity.Employee;
 import java.util.List;
 
 public interface EmployeeService {
-    List<Employee> findAll();
+        List<Employee> findAll();
+
+        Employee findById(int theId);
+
+        Employee save(Employee theEmployee);
+
+        void deleteById(int theId);
+
 }
+
